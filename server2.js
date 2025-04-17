426c9aff8cbdd76444e2a47b23130bf1cffcc6b5
Akshit Nagrale <122260694+gigachad2003@users.noreply.github.com>
2025-04-17 15:34:40 +0530

    Update server2.js

--- server2.js
+++ server2.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { Pool } = require('pg');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
+
 
 const app = express();
 app.use(cors());
